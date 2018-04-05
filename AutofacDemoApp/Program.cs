50a6e65b1108923a3503dc60f33af0acc47fc07d
cherishPhoebe <cherish1224@outlook.com>
2018-04-05 10:45:12 +0800

    autofac demo first commit

---
+++ AutofacDemoApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Autofac;
+
+namespace AutofacDemoApp
+{
+    class Program
+    {
+        private static IContainer Container { get; set; }
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            // 如果注册两个类型一样的接口，解析时使用的时后注册的对象（后入先出）
+            builder.RegisterType<TomorrowWriter>().As<IDateWriter>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+            Container = builder.Build();
+
+            WriteDate();
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+
+        public static void WriteDate() {
+            using (var scope = Container.BeginLifetimeScope()) {
+                IDateWriter write = scope.Resolve<IDateWriter>();
+                write.WriteDate();
+            }
+        }
+    }
+}
---
+++ AutofacDemoApp/TodayWriter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutofacDemoApp
+{
+    public interface IOutput {
+        void Write(string content);
+    }
+
+    public class ConsoleOutput : IOutput {
+        public void Write(string content) {
+            Console.WriteLine(content);
+        }
+    }
+
+    public interface IDateWriter {
+        void WriteDate();
+    }
+    public class TodayWriter : IDateWriter
+    {
+        private IOutput _output;
+
+        public TodayWriter(IOutput output) {
+            this._output = output;
+        }
+        public void WriteDate()
+        {
+            this._output.Write(DateTime.Today.ToShortDateString());
+        }
+    }
+
+    public class TomorrowWriter : IDateWriter {
+        private IOutput _output;
+
+        public TomorrowWriter(IOutput output) {
+            this._output = output;
+        }
+
+        public void WriteDate() {
+            this._output.Write(DateTime.Today.AddDays(1).ToShortDateString());
+        }
+
+    }
+}
